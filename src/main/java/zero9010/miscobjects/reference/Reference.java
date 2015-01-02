f5a3401ebe460d8c059891d0d6ffb3377d5a7bc7
Arceusthepokemon <Arceusthepokemon@hotmail.com>
2015-01-02 18:26:37 -0500

    Add inits and reference class

---
+++ src/main/java/zero9010/miscobjects/reference/Reference.java
@@ -0,0 +1,11 @@
+package zero9010.miscobjects.reference;
+
+/**
+ * Created by Arceusthepokemon on 1/2/2015.
+ */
+public class Reference {
+    public static final String MOD_ID = "miscobjects";
+    public static final String MOD_NAME = "The Geeks's Miscellaneous Objects";
+    public static final String VERSION = "0.0.1";
+
+}
