55b6ed2efab8002606ebc73ea28eeb7e4b51394d
Alexander <xxphantom.zeroxx@gmail.com>
2015-01-02 18:41:49 -0500

    Version 0.0.1

--- src/main/java/zero9010/miscobjects/reference/Reference.java
+++ src/main/java/zero9010/miscobjects/reference/Reference.java
@@ -1,8 +1,5 @@
 package zero9010.miscobjects.reference;
 
-/**
- * Created by Arceusthepokemon on 1/2/2015.
- */
 public class Reference {
     public static final String MOD_ID = "miscobjects";
     public static final String MOD_NAME = "The Geeks's Miscellaneous Objects";
