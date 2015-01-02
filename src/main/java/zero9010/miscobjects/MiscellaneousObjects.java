72548fa397c3bb96672b4577f6d343dfba3b5b61
Alexander <xxphantom.zeroxx@gmail.com>
2015-01-02 18:08:13 -0500

    Version 0.0.1

--- src/main/java/zero9010/miscobjects/MiscellaneousObjects.java
+++ src/main/java/zero9010/miscobjects/MiscellaneousObjects.java
@@ -1,4 +1,18 @@
 package zero9010.miscobjects;
 
-public class MiscellaneousObjects {
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = MiscellaneousObjects.MODID, version = MiscellaneousObjects.VERSION)
+public class MiscellaneousObjects
+{
+
+    public static final String MODID = "miscobjects";
+    public static final String VERSION = "1.0";
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
 }
