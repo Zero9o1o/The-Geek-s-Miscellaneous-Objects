0090496149e3b994b92977a5b2f4dda865f3b71e
Arceusthepokemon <Arceusthepokemon@hotmail.com>
2015-01-02 18:24:40 -0500

    Add inits and reference classs

--- src/main/java/zero9010/miscobjects/MiscellaneousObjects.java
+++ src/main/java/zero9010/miscobjects/MiscellaneousObjects.java
@@ -1,18 +1,25 @@
 package zero9010.miscobjects;
 
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = MiscellaneousObjects.MODID, version = MiscellaneousObjects.VERSION)
+import zero9010.miscobjects.reference.Reference;
+
+@Mod(modid = Reference.MOD_ID, version = Reference.VERSION)
 public class MiscellaneousObjects
 {
 
-    public static final String MODID = "miscobjects";
-    public static final String VERSION = "1.0";
-    @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
+    @Mod.EventHandler
+    public void preinit(FMLPreInitializationEvent event){
+    }
 
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event){
+    }
+
+    @Mod.EventHandler
+    public void postinit(FMLPostInitializationEvent event){
     }
 }
