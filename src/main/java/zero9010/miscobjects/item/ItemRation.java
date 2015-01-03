02f8374241fba1169c3455f67ec171bbcab24071
Alexander <xxphantom.zeroxx@gmail.com>
2015-01-02 19:05:18 -0500

    First item

---
+++ src/main/java/zero9010/miscobjects/item/ItemRation.java
@@ -0,0 +1,11 @@
+package zero9010.miscobjects.item;
+
+import zero9010.miscobjects.reference.Names;
+
+public class ItemRation  extends ItemMO{
+    public ItemRation()
+    {
+        super();
+        this.setUnlocalizedName(Names.Items.RATION);
+    }
+}
---
+++ src/main/java/zero9010/miscobjects/reference/Names.java
@@ -0,0 +1,8 @@
+package zero9010.miscobjects.reference;
+
+public final class Names {
+    public static final class Items
+    {
+        public static final String RATION = "Rations";
+    }
+}
