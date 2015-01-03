f877e5d9545e51cdb272d545ef107e6184e66238
Arceusthepokemon <Arceusthepokemon@hotmail.com>
2015-01-02 20:39:15 -0500

    ration is now a food item

--- src/main/java/zero9010/miscobjects/item/ItemRation.java
+++ src/main/java/zero9010/miscobjects/item/ItemRation.java
@@ -1,11 +1,30 @@
 package zero9010.miscobjects.item;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.item.ItemFood;
+import zero9010.miscobjects.creativetab.creativeTab;
 import zero9010.miscobjects.reference.Names;
 
-public class ItemRation  extends ItemMO{
+public class ItemRation  extends ItemFood{
     public ItemRation()
     {
-        super();
+        super(2, 0.3F,false);
         this.setUnlocalizedName(Names.Items.RATION);
+        setCreativeTab(creativeTab.miscobjects);
     }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerIcons(IIconRegister iconRegister)
+    {
+        itemIcon = iconRegister.registerIcon(this.getUnlocalizedName().substring(this.getUnlocalizedName().indexOf(".") + 1));
+    }
+
+    protected String getUnwrappedUnlocalizedName(String unlocalizedName)
+    {
+        return unlocalizedName.substring(unlocalizedName.indexOf(".") + 1);
+    }
+
 }
