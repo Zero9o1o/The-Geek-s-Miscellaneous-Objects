f19752c236101ec46c875a971543f5a583d77d19
fstobbsie <frazerstobbs@gmail.com>
2015-01-04 03:34:07 +0000

    added steak strips

--- src/main/java/zero9010/miscobjects/init/craftingRecipes.java
+++ src/main/java/zero9010/miscobjects/init/craftingRecipes.java
@@ -3,12 +3,15 @@ package zero9010.miscobjects.init;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import zero9010.miscobjects.item.ItemKnife;
+import zero9010.miscobjects.item.ItemSteakStrips;
 
 public class craftingRecipes {
 
     public static void init(){
 
         GameRegistry.addRecipe(new ItemStack((initItem.ItemKnife), 1), new Object[]{"/  ", " x ", "  x", 'x', Items.iron_ingot, '/', Items.stick});
+        GameRegistry.addRecipe(new ItemStack((initItem.ItemSteakStrips), 3), new Object[]{"/x ","   ","   ", 'x', Items.cooked_beef, '/', initItem.ItemKnife});
 
     }
 
