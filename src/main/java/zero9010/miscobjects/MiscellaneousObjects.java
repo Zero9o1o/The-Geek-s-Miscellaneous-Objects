fa3e41f4a914012370d651682e5c080e4978fcb9
Arceusthepokemon <Arceusthepokemon@hotmail.com>
2015-01-04 02:52:21 -0500

    look how well this works

--- src/main/java/zero9010/miscobjects/MiscellaneousObjects.java
+++ src/main/java/zero9010/miscobjects/MiscellaneousObjects.java
@@ -1,22 +1,32 @@
 package zero9010.miscobjects;
 
 import cpw.mods.fml.common.Mod;
+//import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 import zero9010.miscobjects.init.craftingRecipes;
+//import zero9010.miscobjects.proxy.ClientProxy;
+//import zero9010.miscobjects.proxy.CommonProxy;
+//import zero9010.miscobjects.proxy.Iproxy;
 import zero9010.miscobjects.reference.Reference;
 import zero9010.miscobjects.init.initItem;
+//import zero9010.miscobjects.init.initBlocks;
 
 @Mod(modid = Reference.MOD_ID, version = Reference.VERSION)
 public class MiscellaneousObjects
 {
 
+    //@SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
+    //public static CommonProxy proxy;
+
     @Mod.EventHandler
     public void preinit(FMLPreInitializationEvent event){
 
         initItem.init();
+        //initBlocks.init();
+
 
     }
 
@@ -24,6 +34,8 @@ public class MiscellaneousObjects
     public void init(FMLInitializationEvent event){
 
         craftingRecipes.init();
+
+        //proxy.initRenderers();
 
     }
 
